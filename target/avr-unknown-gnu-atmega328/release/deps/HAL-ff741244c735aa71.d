59fe323564dadfcba82a7badd03de711710e8828
Kylie <149889932+Kir-w@users.noreply.github.com>
2024-10-24 15:13:55 +0200

    Delete target/avr-unknown-gnu-atmega328 directory

--- target/avr-unknown-gnu-atmega328/release/deps/HAL-ff741244c735aa71.d
+++
@@ -1,5 +0,0 @@
-C:\Users\wwuky\Documents\rustprojects\HAL\target\avr-unknown-gnu-atmega328\release\deps\HAL-ff741244c735aa71.elf: src/main.rs
-
-C:\Users\wwuky\Documents\rustprojects\HAL\target\avr-unknown-gnu-atmega328\release\deps\HAL-ff741244c735aa71.d: src/main.rs
-
-src/main.rs:
--- target/avr-unknown-gnu-atmega328/release/deps/cfg_if-e63fffb5ca2809e8.d
+++
@@ -1,7 +0,0 @@
-C:\Users\wwuky\Documents\rustprojects\HAL\target\avr-unknown-gnu-atmega328\release\deps\libcfg_if-e63fffb5ca2809e8.rmeta: C:\Users\wwuky\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-0.1.10\src\lib.rs
-
-C:\Users\wwuky\Documents\rustprojects\HAL\target\avr-unknown-gnu-atmega328\release\deps\libcfg_if-e63fffb5ca2809e8.rlib: C:\Users\wwuky\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-0.1.10\src\lib.rs
-
-C:\Users\wwuky\Documents\rustprojects\HAL\target\avr-unknown-gnu-atmega328\release\deps\cfg_if-e63fffb5ca2809e8.d: C:\Users\wwuky\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-0.1.10\src\lib.rs
-
-C:\Users\wwuky\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-0.1.10\src\lib.rs:
