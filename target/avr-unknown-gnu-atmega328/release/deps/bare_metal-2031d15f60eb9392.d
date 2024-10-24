c9653e13256a66f16e0cf981c9dfd20ced55e40f
Kylie <149889932+Kir-w@users.noreply.github.com>
2024-10-24 15:12:16 +0200

    Add files via upload

---
+++ target/avr-unknown-gnu-atmega328/release/deps/bare_metal-2031d15f60eb9392.d
@@ -0,0 +1,7 @@
+C:\Users\wwuky\Documents\rustprojects\HAL\target\avr-unknown-gnu-atmega328\release\deps\libbare_metal-2031d15f60eb9392.rmeta: C:\Users\wwuky\.cargo\registry\src\index.crates.io-6f17d22bba15001f\bare-metal-0.2.5\src\lib.rs
+
+C:\Users\wwuky\Documents\rustprojects\HAL\target\avr-unknown-gnu-atmega328\release\deps\libbare_metal-2031d15f60eb9392.rlib: C:\Users\wwuky\.cargo\registry\src\index.crates.io-6f17d22bba15001f\bare-metal-0.2.5\src\lib.rs
+
+C:\Users\wwuky\Documents\rustprojects\HAL\target\avr-unknown-gnu-atmega328\release\deps\bare_metal-2031d15f60eb9392.d: C:\Users\wwuky\.cargo\registry\src\index.crates.io-6f17d22bba15001f\bare-metal-0.2.5\src\lib.rs
+
+C:\Users\wwuky\.cargo\registry\src\index.crates.io-6f17d22bba15001f\bare-metal-0.2.5\src\lib.rs:
