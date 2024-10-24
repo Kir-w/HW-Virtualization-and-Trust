83feff950661c446e20689c6c40285f6b82e37cf
Kylie <149889932+Kir-w@users.noreply.github.com>
2024-10-24 15:16:42 +0200

    Add files via upload

---
+++ target/avr-unknown-gnu-atmega328/release/deps/HAL-ff741244c735aa71.d
@@ -0,0 +1,5 @@
+C:\Users\wwuky\Documents\rustprojects\HAL\target\avr-unknown-gnu-atmega328\release\deps\HAL-ff741244c735aa71.elf: src/main.rs
+
+C:\Users\wwuky\Documents\rustprojects\HAL\target\avr-unknown-gnu-atmega328\release\deps\HAL-ff741244c735aa71.d: src/main.rs
+
+src/main.rs:
