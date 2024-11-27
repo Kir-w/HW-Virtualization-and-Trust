36ea28b594f7bf79aece407fb56a7ea15348ee03
Maiaroca <146460693+Maiaroca@users.noreply.github.com>
2024-11-27 19:59:06 +0100

    Update mod.rs

--- src/spi/mod.rs
+++ src/spi/mod.rs
@@ -7,3 +7,8 @@ pub trait Spi {
     fn read_byte(&self) -> u8;
     fn transfer(&self, data: &[u8]) -> Vec<u8>; // Ajout pour transférer des données
 }
+// Configuration SPI partagée entre toutes les implémentations
+pub struct SpiConfig {
+    pub mode: u8,       // Mode SPI : 0, 1, 2 ou 3
+    pub frequency: u32, // Fréquence en Hz
+}
