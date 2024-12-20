f8f96a13514c06ede806a62d0967edf1cf155344
Maiaroca <146460693+Maiaroca@users.noreply.github.com>
2024-12-20 22:45:37 +0100

    Delete target directory

--- target/release/build/bare-metal-a6af80efcea44109/build_script_build-a6af80efcea44109.d
+++
@@ -1,5 +0,0 @@
-C:\hw\P2\projet\target\release\build\bare-metal-a6af80efcea44109\build_script_build-a6af80efcea44109.exe: C:\Users\arian\.cargo\registry\src\index.crates.io-6f17d22bba15001f\bare-metal-0.2.5\build.rs
-
-C:\hw\P2\projet\target\release\build\bare-metal-a6af80efcea44109\build_script_build-a6af80efcea44109.d: C:\Users\arian\.cargo\registry\src\index.crates.io-6f17d22bba15001f\bare-metal-0.2.5\build.rs
-
-C:\Users\arian\.cargo\registry\src\index.crates.io-6f17d22bba15001f\bare-metal-0.2.5\build.rs:
