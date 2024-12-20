5ca9ea4a6d62d4c02adcfe13b9fdefabd7ce00d1
Kylie <149889932+Kir-w@users.noreply.github.com>
2024-12-20 22:07:53 +0100

    Update atmega.rs

--- src/atmega.rs
+++ src/atmega.rs
@@ -1,43 +1,45 @@
-pub mod ard_gpio;
-use ard_gpio::{Gpio, PinMode};
+pub mod esp_gpio;
+use esp_gpio::{Gpio, PinMode};
 
-pub mod ard_usart;
-use ard_usart::Usart;
+pub mod esp_usart;
+use esp_usart::Usart;
 
-pub mod ard_spi;
-use ard_spi::Spi;
+pub mod esp_spi;
+use esp_spi::Spi;
 
-pub mod ard_i2c;
-use ard_i2c::I2cInterface;
+pub mod esp_i2c;
+use esp_i2c::I2cInterface;
 
-pub fn fn_atm()-> ! {
+pub fn fn_esp() -> ! {
     let gpio = Gpio::new();
-    gpio.pin_mode(5, PinMode::Output); // PORTB5, Arduino Pin 13
-    Usart::init(9600); // Initialise USART
-    //Spi::init_master();//Initialise SPI
-    I2cInterface::init(); // initialise i2c
+    gpio.pin_mode(2,PinMode::Output); // Configurer GPIO2 -> led embarquée
+    Spi::init_master();
+    Usart::init(9600); // Initialiser USART
+    I2cInterface::init(400_000); // Initialiser I2C avec une fréquence de 400kHz
 
     loop {
-        gpio.digital_write(5, true); // Allume la LED
-        Usart::send(1); // Envoie le signal via usart
-        //Spi::send(0x11); // Envoie les données via spi
-        let sensor_data = I2cInterface::read_sensor(0x40, 0x00); // Lit le capteur via I2C
+        // Allumer la LED
+        gpio.digital_write(2,true);
+        Usart::send(1); // Envoyer un signal via USART
+        Spi::send(0x11); // Envoyer des données via SPI
+        let sensor_data = I2cInterface::read_sensor(0x40, 0x00); // Lire un capteur via I2C
 
         delay();
 
-        gpio.digital_write(5, false); // Eteint la LED
-        Usart::send(0); // Envoie le signal
-        //Spi::receive(); // Lit la réponse
+        // Éteindre la LED
+        gpio.digital_write(2,false);
+        Usart::send(0); // Envoyer un autre signal
+        Spi::receive(); // Lire la réponse SPI
         delay();
 
-        // Traiter les données du capteur I2C (par exemple, envoyer via USART)
+        // Traiter les données du capteur I2C
         Usart::send(sensor_data);
-
-        }
+    }
 }
 
+/// Implémentation d'une fonction de délai basique
 fn delay() {
-    for _ in 0..400000 {
-        unsafe { core::ptr::read_volatile(&0) };
+    for _ in 0..400_000 {
+        // Attente simple
     }
 }
