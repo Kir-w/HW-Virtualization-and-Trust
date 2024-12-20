8c5fb89337ce427ec122701943c0458edd8a841f
Kylie <149889932+Kir-w@users.noreply.github.com>
2024-12-20 22:11:11 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,41 @@
-#![no_std] 
+#![no_std]
 #![no_main]
 
 use core::panic::PanicInfo;
 
-//on appelle la fonction fn_atm depuis le fichier atmega si le feature utiliser est atmega
+// Sélection des modules selon la feature acctivée
+// Remettre le default features dans le cargo toml pour lancer directement l'atmega
+// Sinon faire la commande suivante pour lancer l'atmega:
+// cargo +nightly build -Z build-std=core --target avr-unknown-gnu-atmega328 --release --features atmega
+// ou la commande suivante pour lancer l'esp32
+// cargo +nightly build -Z build-std=core --target avr-unknown-gnu-atmega328 --release --features esp32
 #[cfg(feature = "atmega")]
 mod atmega;
-
 #[cfg(feature = "atmega")]
 use atmega::fn_atm;
 
-//on appelle la fonction fn_esp depuis le fichier esp32 si le feature utiliser est esp32
 #[cfg(feature = "esp32")]
 mod esp32;
-
 #[cfg(feature = "esp32")]
 use esp32::fn_esp;
 
-// Pour gèrer les erreurs
+// Panic handler
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
-#[no_mangle]
-pub extern "C" fn main()->!{
-    #[cfg(feature = "atmega")] 
-    fn_atm();
-    loop {
-    }
 
-    #[cfg(feature = "esp32")]
-    fn_esp();
-    loop {
+// Fonction principale
+#[no_mangle]
+fn main() -> ! {
+    // ctf_if permet de sélectionner une feature directement au moment de la compilation
+    cfg_if::cfg_if! {
+        if #[cfg(feature = "atmega")] {
+            fn_atm();
+        } else if #[cfg(feature = "esp32")] {
+            fn_esp();
+        } else {
+            compile_error!("Aucune feature valide n'est activée : activez 'atmega' ou 'esp32'.");
+        }
     }
 }
